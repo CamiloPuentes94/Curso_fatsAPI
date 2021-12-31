0acf684e01defb24eab3bf6e9bd6df03f1606dee
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2021-12-31 12:41:07 -0500

    Validations_query: se realiza validacion de query, se crea funcion para la validacion de los query

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Python
 from typing import Optional
+from fastapi.param_functions import Query
 
 # Pydantic
 from pydantic import BaseModel
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -29,3 +30,12 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):   #esta clase me dice que es de tipo body, cuando en los parametros tienen ... quiere decir que es obligatorio
     return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None,min_length=1,max_length=50),
+    age: str = Query(...)
+):
+    return {name: age}
