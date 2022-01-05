6e4db67b6de746f353edcfd1ec16a10bd1f069f8
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2022-01-05 11:47:52 -0500

    Se realiza validaciones de body con un nuevo path opetaions de put

--- main.py
+++ main.py
@@ -62,3 +62,17 @@ def show_person(
         ) 
 ):
     return {person_id: "It exist!"}
+
+# validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title= "Person ID",
+        description= "This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...)
+):
+    return person
