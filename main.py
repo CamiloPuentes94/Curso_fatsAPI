d438d0f1d5dbb12fd9668e3bd74d895c15b4a1d6
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2022-01-17 16:53:14 -0500

    se agrga funcion contact y se definen sus variables y se returna el valor de user_agent. se importa de pydantic Emailstr y de fastapi Header y Cookie

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from enum import Enum
 # Pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 from  fastapi import status
 
 app = FastAPI()
@@ -165,3 +166,23 @@ def login(username: str = Form(...), password: str = Form(...)):
     path= "/contact",
     status_code=status.HTTP_200_OK
 )
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length = 20,
+        min_length = 1 
+    ),
+    last_name: str = Form(
+        ...,
+        max_length = 20,
+        min_length = 1 
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length= 20
+    ),
+    user_agent: Optional[str] = Header(default= None),
+    ads: Optional[str] = Cookie(default= None)
+):
+    return user_agent
